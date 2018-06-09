aa2df0f5c9d3ffe96be5cf1924590e96209e5da9
DESKTOP-201RB4R\anbba <jackhacktac@gmail.com>
2018-06-09 18:55:03 -0400

    i dont even know anymore

---
+++ FirstPersonShooter/Source/FirstPersonShooter/FirstPersonShooterHUD.h
@@ -0,0 +1,25 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once 
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "FirstPersonShooterHUD.generated.h"
+
+UCLASS()
+class AFirstPersonShooterHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	AFirstPersonShooterHUD();
+
+	/** Primary draw call for the HUD */
+	virtual void DrawHUD() override;
+
+private:
+	/** Crosshair asset pointer */
+	class UTexture2D* CrosshairTex;
+
+};
+
